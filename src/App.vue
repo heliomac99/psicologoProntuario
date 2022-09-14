642c60819e9f014a0cfd8a2433ebb31c432a33a2
heliomac99 <80364554+heliomac99@users.noreply.github.com>
2022-09-13 21:21:39 -0300

    App.vue com a navbar e os links para as telas

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <router-link to="/" class="nav-link" >Home</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/Paciente" class="nav-link" >Paciente</router-link>
+                </li>
+            </ul>
+        </div>
+
+    </nav>
+    <router-view></router-view>
+
 </template>
 
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+    li {
+        margin:10px;  
+    }
+    nav {
+        margin-bottom:20px;
+    }
+    a {
+        text-decoration: none;
+    }
 </style>
