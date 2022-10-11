435cc2dc2889582b1a0f21b34f31c48b222c9297
attiilla <atilappl@gmail.com>
2022-10-10 22:03:45 -0300

    Rota login, tela login inicial, link para login na navbar

---
+++ client/src/views/LoginScreen.vue
@@ -0,0 +1,30 @@
+<template>
+    <br>
+    <input id="email" class="form-control" placeholder="Email" style="margin-right: 10px;margin-left: 80px; width: 500px;">
+    <br>
+    <input id="password" class="form-control" placeholder="Senha" type="password" style="margin-right: 10px; margin-left: 80px; width: 500px;">
+    <br>
+    <button type="button" class="btn btn-primary primaryColorBtn" @click="Entrar" style="margin-right: 10px; margin-left: 205px; width: 250px;">
+        Entrar
+    </button>
+</template>
+
+<script>
+//import axios from 'axios'
+  export default {
+    name: 'LoginScreen',
+    data(){
+        return{
+
+        }
+    },
+    methods: {
+        Entrar(){
+
+        },
+  }
+}
+</script>
+
+<style>
+</style>
