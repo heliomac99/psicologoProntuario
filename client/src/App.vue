4aae5b0e35cbfc5622d7405b6cafc8ef78fd309c
attiilla <atilappl@gmail.com>
2022-10-12 20:52:03 -0300

    login avanços

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 ﻿<template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light"  >
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -28,6 +28,11 @@
 
 export default {
         name: 'App',
+        computed:{
+            hideNavbar(){
+                return (this.$router.name == 'LogIn');
+            }
+        }
 
 }
 </script>
--- client/src/views/LoginScreen.vue
+++ client/src/views/LoginScreen.vue
@@ -1,27 +1,35 @@
 <template>
-    <br>
-    <input id="email" class="form-control" placeholder="Email" style="margin-right: 10px;margin-left: 80px; width: 500px;">
-    <br>
-    <input id="password" class="form-control" placeholder="Senha" type="password" style="margin-right: 10px; margin-left: 80px; width: 500px;">
-    <br>
-    <button type="button" class="btn btn-primary primaryColorBtn" @click="Entrar" style="margin-right: 10px; margin-left: 205px; width: 250px;">
-        Entrar
+    <div>
+        <br>
+        <input id="email" class="form-control" placeholder="Email" v-model="email" style="margin-right: 10px;margin-left: 80px; width: 500px;">
+        <br>
+        <input id="password" class="form-control" placeholder="Senha" v-model="senha" type="password" style="margin-right: 10px; margin-left: 80px; width: 500px;">
+        <br>
+        <button type="button" class="btn btn-primary primaryColorBtn" @click="Entrar" style="margin-right: 10px; margin-left: 205px; width: 250px;">
+            Entrar
     </button>
+    </div>
 </template>
 
 <script>
-//import axios from 'axios'
+import axios from 'axios'
+import HomeView from './Home.vue'
   export default {
     name: 'LoginScreen',
+    components: HomeView,
     data(){
         return{
-
+            email: "",
+            senha: ""
         }
     },
     methods: {
         Entrar(){
-
-        },
+            // tapa buraco enquanto não consigo conversar com o banco de dados para verificar a senha e o email
+            axios.post('http://localhost:4000/login', {login:'teste',senha:'teste'}).then( (result) => {
+                        console.log(result)
+                   })
+        }
   }
 }
 </script>
